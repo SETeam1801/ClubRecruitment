19131808cd56f0534591eaafbce56c5405d6bdad
foolishkylin <foolishkylin@gmail.com>
2020-05-29 16:43:15 +0800

    fixed the stop & delete bug

--- src/cr_backend/clubRecruitment/views/manager_side.py
+++ src/cr_backend/clubRecruitment/views/manager_side.py
@@ -96,7 +96,7 @@ def find_apps(request, _admin, dept_id):
                     rep['data']['students'].append(app_data)
                 rep['data']['round'] = dept.current_round
                 rep['data']['stuNum'] = len(apps)
-                rep['data']['passNum'] = len([app for app in apps if app.stu_status == 2])
+                rep['data']['passNum'] = len([app for app in apps if app.stu_status != 0])
                 rep['data']['deleteNum'] = len([app for app in apps if app.stu_status == 0])
         except Department.DoesNotExist:
             rep = settings.REP_STATUS[211]
@@ -442,20 +442,15 @@ def next_round(request, _admin, dept_id):
 @post_log
 def stop_apply(_request, req_js, _admin):
     try:
-        dept = Department.objects.get(pk=req_js['deptId'])
-        apps = Recruitment.objects.filter(Department=dept)
-        for stu_id in req_js['stopList']:
-            stu = Student.objects.get(pk=stu_id)
-            app = apps.get(Student=stu)
+        for app_id in req_js['stopList']:
+            app = Recruitment.objects.get(Department=app_id)
             app.stu_status = 0
             app.save()
         rep = settings.REP_STATUS[100]
     except KeyError:
         rep = settings.REP_STATUS[300]
-    except Department.DoesNotExist or Recruitment.DoesNotExist or Student.DoesNotExist:
+    except Recruitment.DoesNotExist:
         rep = settings.REP_STATUS[211]
-    except Exception:
-        rep = settings.REP_STATUS[666]
     return JsonResponse(rep, safe=False)
 
 
@@ -464,15 +459,12 @@ def stop_apply(_request, req_js, _admin):
 @post_log
 def delete_apply(_request, req_js, _admin):
     try:
-        dept = Department.objects.get(pk=req_js['deptId'])
-        apps = Recruitment.objects.filter(Department=dept)
-        for stu_id in req_js['deleteList']:
-            stu = Student.objects.get(pk=stu_id)
-            apps.get(Student=stu).delete()
+        for app_id in req_js['deleteList']:
+            Recruitment.objects.get(pk=app_id).delete()
         rep = settings.REP_STATUS[100]
     except KeyError:
         rep = settings.REP_STATUS[300]
-    except Department.DoesNotExist or Recruitment.DoesNotExist:
+    except Recruitment.DoesNotExist:
         rep = settings.REP_STATUS[211]
     return JsonResponse(rep, safe=False)
 
